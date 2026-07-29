caf470b7674c8ff51c22a906e14e2ba0b4c24ab8
xinghaix <xinghaix@github.com>
2026-07-29 10:29:34 +0800

    chore(cpa-manager-plus): bump version to 0.5.1

--- plugins/cpa-manager-plus/go/main_test.go
+++ plugins/cpa-manager-plus/go/main_test.go
@@ -40,7 +40,7 @@ func TestPluginRegistrationExposesUsagePlugin(t *testing.T) {
 	if !registration.Capabilities.ManagementAPI || !registration.Capabilities.UsagePlugin {
 		t.Fatalf("capabilities = %#v", registration.Capabilities)
 	}
-	if registration.Metadata.Version != "0.5.0" {
+	if registration.Metadata.Version != "0.5.1" {
 		t.Fatalf("version = %s", registration.Metadata.Version)
 	}
 	if registration.SchemaVersion != 1 {
