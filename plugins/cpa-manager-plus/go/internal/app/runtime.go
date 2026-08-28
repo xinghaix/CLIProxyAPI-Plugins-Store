0885b0ce108b8c627356a9e6f0d6fe3cf41fcb53
xinghaix <xinghaix@github.com>
2026-08-28 12:59:14 +0800

    chore(cpa-manager-plus): bump version to 0.5.17

--- plugins/cpa-manager-plus/go/internal/app/runtime.go
+++ plugins/cpa-manager-plus/go/internal/app/runtime.go
@@ -24,7 +24,7 @@ import (
 	"github.com/xinghaix/CLIProxyAPI-Plugins-Store/plugins/cpa-manager-plus/go/internal/store"
 )
 
-const runtimeVersion = "0.5.16"
+const runtimeVersion = "0.5.17"
 
 type connection struct {
 	BaseURL       string `json:"cpaBaseUrl"`
